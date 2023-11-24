edc2ca1d344443245c64e72f1b5b3bbb74854f2f
dvonix <fakhrialbari.pro@gmail.com>
2023-11-24 21:22:31 +0700

    add-vuetify

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <v-container>
+        <v-btn @click="showAlert('Hello World')">Click Me</v-btn>
+      </v-container>
+    </v-app>
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+
+function showAlert(message : string) {
+  window.alert(message)
+}
+
+</script>
